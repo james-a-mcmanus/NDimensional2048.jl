b51fe8ecf1730a3d2c5127b7c576f6dcb3268581
james-a-mcmanus <james.alexander.mcmanus@gmail.com>
2021-08-31 16:41:07 +0100

    basic sparse board works

--- 2048.jl
+++ 2048.jl
@@ -1,39 +1,88 @@
-import Base: size, getindex, setindex
+import Base: size, getindex, setindex, show
+using SparseArrays
+using StatsBase
 
-mutable struct Board{T} <: AbstractArray{T,2}
+abstract type AbstractBoard{T,N} <: AbstractArray{T,N} end
+
+mutable struct Board{T} <: AbstractBoard{T,2}
 	plates::Matrix{T}
 	frees::Array{CartesianIndex{2}}
 end
 
-function Board(N::Int)
-	plates = zeros(Int, N, N)
-	frees = reduce(vcat, collect(CartesianIndices(plates)))
-	return Board(plates, frees)
+struct SparseBoard{T} <: AbstractBoard{T,2}
+	plates::SparseMatrixCSC{T}
 end
 
-Base.size(b::Board) = size(b.plates)
+const StartingFillProportion = 0.5
 
-Base.getindex(b::Board, i...) = getindex(b.plates,i...)
-Base.setindex!(b::Board, i...) = setindex!(b.plates,i...)
+function SparseBoard(N::Int)
+	plates = spzeros(Int, N,N)
+	return SparseBoard(plates)
+end
+
+function Board(N::Int)
+	plate = zeros(Int, N, N)
+	free = reduce(vcat, collect(CartesianIndices(plate)))
+	b = Board(plate, free)
+	start_inds = StatsBase.sample(CartesianIndices(b), floor(Int, N^2 * StartingFillProportion))
+	add_number!(b, start_inds)
+	return b
+end
+
+Base.size(b::AbstractBoard) = size(b.plates)
+
+Base.getindex(b::AbstractBoard, i...) = getindex(b.plates,i...)
+Base.setindex!(b::AbstractBoard, i...) = setindex!(b.plates,i...)
 
 frees(b) = iszero.(b)
 frees(b::Board) = b.frees
-plates(b::Board) = b.plates
+frees(b::SparseBoard) = findall(iszero, b) # slow
 
+plates(b::AbstractBoard) = b.plates
+
+function rand_free_plate(b::Board)
+	free_plates = frees(b)
+	isempty(free_plates) && return nothing
+	return rand(1:length(free_plates))
+end
+
+function rand_free_plate(b::SparseBoard; limit=1000)
+	counter = 0
+	while true
+		i = rand(1:length(b))
+		counter += 1
+		counter > limit && break
+		iszero(b[i]) && return i 
+	end
+	return rand(findall(!iszero, b))
+end
 
 function next_turn!(board)
-	free_plates = frees(board)
-	isempty(free_plates) && return 1
-	i = rand(1:length(free_plates))
-	chosen_plate = free_plates[i]
-	add_number!(board, chosen_plate)
-	deleteat!(free_plates, i)
+	plate_ind = rand_free_plate(board)
+	isnothing(plate_ind) && return 1
+	add_number!(board, plate_ind)
 	return 0
 end
 
-function add_number!(board, ind)
-	plates(board)[ind] = 2
+function add_number!(board::Board, ind::Int)
+	plates(board)[frees(board)[ind]] = new_number(board)
+	deleteat!(frees(board), ind)
 end
+
+function add_number!(board::Board, ind::CartesianIndex)
+	plates(board)[ind] = new_number(board)
+	freeind = findfirst(x->x==ind, frees(board))
+	!isnothing(freeind) && deleteat!(frees(board), freeind)
+end
+
+function add_number!(board::SparseBoard, ind)
+	plates(board)[ind] = new_number(board)
+end
+function add_number!(b::Board, ind::AbstractArray)
+	plates(b)[ind] .= new_number(b)
+end
+
+new_number(a) = 2
 
 function move!(board, iterover, rev)
 	board_length = size(board,1)
@@ -43,6 +92,11 @@ function move!(board, iterover, rev)
 		squeeze_slice!(slice, rev)
 	end	
 end
+
+function move!(board::SparseBoard, iterover, rev)
+
+end
+
 
 up!(board) = move!(board, eachcol, true)
 down!(board) = move!(board, eachcol, false)
@@ -119,4 +173,9 @@ function make_move(board, key)
 	key == '\e'
 		left!(board)
 	#key = 
+end
+
+function i_to_cart(a, i)
+	inds = CartesianIndices(a)
+	return inds[i]
 end
